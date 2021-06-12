864544452f9bede041b541b87ed7ad6858cd9223
Kevin Torres <kdtorresv@hotmail.com>
2021-06-12 20:08:53 +0200

    Returning requests answers

--- internal/platform/server/handler/repeat/repeater.go
+++ internal/platform/server/handler/repeat/repeater.go
@@ -1,33 +1,44 @@
 package repeat
 
 import (
+	"encoding/json"
 	routes "github.com/mrandycodes/kyber/internal"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"strings"
 )
+
+type repetitionResponse struct {
+	Path string `json:"path"`
+	Status string `json:"status"`
+	Response string `json:"response"`
+}
 
 func RepetitionHandler(repository routes.RoutesRepository) func(http.ResponseWriter, *http.Request) {
 	return func(res http.ResponseWriter, req *http.Request) {
 		res.Header().Set("Content-Type", "application/json")
 
+		responses := []repetitionResponse{}
 		client := &http.Client{}
 		path := strings.TrimPrefix(req.RequestURI, "/api")
-		log.Println(path)
 		method := req.Method
 		body := req.Body
 
 		for _, route := range repository.List() {
-			log.Println("Request to: " + route.Value() + path)
 			newRequest, _ := http.NewRequest(method, route.Value() + path, body)
 
 			response, _ := client.Do(newRequest)
 
 			body, _ := ioutil.ReadAll(response.Body)
-			log.Println("Response: " + response.Status + " Body: " + string(body))
+
+			responses = append(responses, repetitionResponse{
+				Path: route.Value() + path,
+				Status: response.Status,
+				Response: string(body),
+			})
 		}
 
 		res.WriteHeader(http.StatusOK)
+		json.NewEncoder(res).Encode(responses)
 	}
 }
