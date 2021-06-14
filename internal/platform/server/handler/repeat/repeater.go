ecbf9588568b79487226878c05a75a905742ebcb
Kevin Torres <kdtorresv@hotmail.com>
2021-06-14 18:54:02 +0200

    Adding go routines

--- internal/platform/server/handler/repeat/repeater.go
+++ internal/platform/server/handler/repeat/repeater.go
@@ -2,9 +2,11 @@ package repeat
 
 import (
 	"encoding/json"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"sync"
 
 	routes "github.com/mrandycodes/kyber/internal"
 )
@@ -19,31 +21,47 @@ func RepetitionHandler(repository routes.RoutesRepository) func(http.ResponseWri
 	return func(res http.ResponseWriter, req *http.Request) {
 		res.Header().Set("Content-Type", "application/json")
 
-		responses := []repetitionResponse{}
-		client := &http.Client{}
+		routes := repository.List()
+		numberOfRoutes := len(routes)
+		var responses = make([]repetitionResponse, numberOfRoutes)
 		path := strings.TrimPrefix(req.RequestURI, "/api")
 		method := req.Method
-		body := req.Body
+		bodyInBytes, _ := ioutil.ReadAll(req.Body)
+		body := strings.NewReader(string(bodyInBytes))
 		headers := req.Header.Clone()
 
-		for _, route := range repository.List() {
-			newRequest, _ := http.NewRequest(method, route.Value()+path, body)
-			for key, value := range headers {
-				newRequest.Header.Add(key, value[len(value)-1])
-			}
+		wg := &sync.WaitGroup{}
+		wg.Add(numberOfRoutes)
+		for i, route := range routes {
+			go func(wg *sync.WaitGroup, i int) {
+				responses[i] = doRequest(method, route, path, body, headers)
+				wg.Done()
+			}(wg, i)
 
-			response, _ := client.Do(newRequest)
-
-			body, _ := ioutil.ReadAll(response.Body)
-
-			responses = append(responses, repetitionResponse{
-				Path:     route.Value() + path,
-				Status:   response.Status,
-				Response: string(body),
-			})
 		}
+
+		wg.Wait()
 
 		res.WriteHeader(http.StatusOK)
 		json.NewEncoder(res).Encode(responses)
+	}
+}
+
+func doRequest(method string, route routes.Route, path string, body io.Reader, headers http.Header) repetitionResponse {
+	client := &http.Client{}
+
+	newRequest, _ := http.NewRequest(method, route.Value()+path, body)
+	for key, value := range headers {
+		newRequest.Header.Add(key, value[len(value)-1])
+	}
+
+	response, _ := client.Do(newRequest)
+
+	newResponseBody, _ := ioutil.ReadAll(response.Body)
+
+	return repetitionResponse{
+		Path:     route.Value() + path,
+		Status:   response.Status,
+		Response: string(newResponseBody),
 	}
 }
